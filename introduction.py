acd6343ed50fe216b304cfdef1c73191c9966ca0
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2022-10-07 17:56:53 +0300

    Add files via upload
    a simple introduction to python

---
+++ introduction.py
@@ -0,0 +1,64 @@
+'''
+introduction to python: Python is an intergrated, object-oriented high level language.
+python interpreter converts code into binary code
+Intergrated Development Environment ie PyCham(.py extension), VSC.
+'''
+
+# Mathematical operations 
+#addition
+z=2+3
+print(z)
+
+#subtraction
+y=9-8
+print(y)
+
+#division to get decimal 
+x=8/4
+print(x)
+
+#division with no remainder 
+a=8//4
+print(a)
+
+#multiplication
+b=4*5
+print(b)
+
+#using mixed operators
+c=(3+6)-3
+print(c)
+
+#using power 
+d= 4**3
+print(d)
+
+#using modulus
+e=10%3
+print(e)
+# ....................................................................................................................................
+
+
+#USING STRINGS
+names ="mark","John","Peter"
+print(names)
+
+#to print double quotes within a string use single quotes at the beginning and end of string.
+print('Navin "Laptop".')
+
+#use backward slash \ to use a predefined symbol such as (')
+print('Navin\'s "Laptop".')
+
+print("Navin \"Laptop\".")
+print("Navin\'s \"Laptop\".")
+
+
+#To print a value multiple times and use \n to send to next line 
+print(10*"\nDrimmi")
+
+#using Raw string: r tells the interpretor not to function the \n fuction 
+print(r"\nDrimmi")  
+
+
+
+
