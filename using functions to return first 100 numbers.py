fd17a6d307cdb249c01ad1cbfebdd1db4575278c
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2022-10-07 21:35:53 +0300

    Add files via upload
    A program to print the first 100 numbers using function(in-built)

---
+++ using functions to return first 100 numbers.py
@@ -0,0 +1,27 @@
+#A program to print the first 100 numbers using function(in-built)
+
+from operator import itruediv
+
+
+class LoopIn:
+    def __init__(self,first,end):
+        self.first = first
+        self.end= end
+        
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.first <= self.end:
+            x=self.first
+            self.first+=1
+            return x
+        else:
+            raise StopIteration 
+        
+def main():
+    loop = LoopIn(0,100)
+    for i in loop:
+        print(i)
+        
+main()
+    
