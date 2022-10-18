9318dd345a86c51a8bf922089e15eed616e695c8
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2022-10-18 23:38:27 +0300

    Add files via upload
    using functions to a summary of users inputs having an assumption of all inputs having a birth_year of 2002

---
+++ attempt.py
@@ -0,0 +1,19 @@
+def person(name,birth_year,weight):
+    age = 2002-birth_year
+    if name == "": #if no name has been entered
+        print("\n name : name" , "\n weight : ",weight, "\n birthdate : ",birth_year)
+        print("\n age" , age)
+    else:
+        print("\n name : ", name ,"\n weight : ", weight, "\n birthdate : ",birth_year,"\n age" , age)
+        
+def main():
+    name = input("Enter the name :") #let the user enter their name
+    weight = float(input("Enter the weight: "))
+    birth_year = 2002           
+    #making all the users have same birth_date of 2002
+    
+    person(name,birth_year, weight)
+    
+main()
+        
+              
