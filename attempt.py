a23bccb7e02f06a0a083cebe4b45d193475c6c2a
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2022-11-07 23:00:09 +0300

    Update attempt.py

--- attempt.py
+++ attempt.py
@@ -1,3 +1,4 @@
+//a python program to 
 def person(name,birth_year,weight):
     age = 2002-birth_year
     if name == "": #if no name has been entered
@@ -9,7 +10,7 @@ def person(name,birth_year,weight):
 def main():
     name = input("Enter the name :") #let the user enter their name
     weight = float(input("Enter the weight: "))
-    birth_year = 2002           
+    birth_year = 2002     //is a fixed year       
     #making all the users have same birth_date of 2002
     
     person(name,birth_year, weight)
