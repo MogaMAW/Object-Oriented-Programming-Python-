c4cf9e204a846d3ee235438c2e401094058c7fee
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-03-07 17:11:25 +0300

    Add files via upload

---
+++ programToCheckForAddiction.py
@@ -0,0 +1,11 @@
+def social_media_addiction_level(time_spent_on_social_media: int) -> str:
+    if time_spent_on_social_media < 2:
+        return "Low"
+    elif time_spent_on_social_media < 4:
+        return "Moderate"
+    else:
+        return "High"
+# Test the function
+print(social_media_addiction_level(1)) # Low
+print(social_media_addiction_level(3)) # Moderate
+print(social_media_addiction_level(5)) # High
