3f06a5ccdf49f9ff19c90a871d7c1f66e36a08a0
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-03-13 14:11:47 +0300

    Update leapyearOrnot.py

--- leapyearOrnot.py
+++ leapyearOrnot.py
@@ -6,7 +6,9 @@ def LeapYear(Year):
   if((Year % 400 == 0) or  
      (Year % 100 != 0) and  
      (Year % 4 == 0)):   
-    print("The given Year is a leap year");  
+    print("The given Year is a leap year"); 
+    
+    # Else it is not a leap year  
   else:  
     print ("The given Year is not a leap year")  
 Year = int(input("Enter the year to check whether a leap year or not: "))  
