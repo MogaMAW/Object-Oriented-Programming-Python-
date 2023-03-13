291cbeb597fd1db7c86a56c4f89451e88d76ed7f
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-03-13 13:28:39 +0300

    Update oddOrEven.py

--- oddOrEven.py
+++ oddOrEven.py
@@ -8,6 +8,9 @@ if (int(num1) % 2) == 0:
    print("{0} is Even number".format(num1))  
 else:  
    print("{0} is Odd number".format(num1))  
+
+# The output after running this code should be 
+
    
 
 
