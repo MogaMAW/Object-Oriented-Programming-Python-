4e8c7e958ab1c58b8a251b28511c4d003be53dfc
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-03-14 13:49:56 +0300

    Update vowelInString.py

--- vowelInString.py
+++ vowelInString.py
@@ -3,7 +3,7 @@
 
 def get_vowels(String):
     return [each for each in String if each in "aeiou"]
-get_string1 = "hello" 
+get_string1 = "hello" # ['e', 'o']
 get_string2 = "python is fun" 
 get_string3 = "coding compiler" 
 get_string4 = "12345xyz" 
