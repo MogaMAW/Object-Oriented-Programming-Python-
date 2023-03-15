40e56c787592f7526e6c5252f1da14cdd9071b05
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-03-15 09:21:08 +0300

    Add files via upload
    A Python program to print all prime number in an interval entered by the user

---
+++ printPrimeInterval.py
@@ -0,0 +1,20 @@
+def disp_Prime(num1, num2):
+	prime_List = []
+	for i in range(num1, num2):
+		if i == 0 or i == 1:
+			continue
+		else:
+			for j in range(2, int(i/2)+1):
+				if i % j == 0:
+					break
+			else:
+				prime_List.append(i)
+	return prime_List
+
+starting_Num = int(input('Enter the starting range: '))
+ending_Num = int(input('Enter the ending range: '))
+lst = disp_Prime(starting_Num, ending_Num)
+if len(lst) == 0:
+	print("There are no prime numbers in this range")
+else:
+	print("The prime numbers in the given range are: ", lst)
