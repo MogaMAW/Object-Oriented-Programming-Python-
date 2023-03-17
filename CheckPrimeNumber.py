305790c35feb72376645a11f284143cc42f066b1
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-03-17 16:57:21 +0300

    Update CheckPrimeNumber.py

--- CheckPrimeNumber.py
+++ CheckPrimeNumber.py
@@ -5,7 +5,7 @@ def PrimeChecking(num):
     if num > 1:  
           # For look to iterate over the given number  
         for i in range(2, int(num/2) + 1):  
-             # Condition to check if the given number is divisible  
+             # Condition to check if the given number is divisible.   
             if (num % i) == 0:  
                    #If divisible by any number it's not a prime number
                 print("The number ",num, "is not a prime number")  
